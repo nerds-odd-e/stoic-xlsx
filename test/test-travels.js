d513fc1473a84efb6a9576b0c126d8cd02dc7b33
Hugues Malphettes <hmalphettes@gmail.com>
2014-01-20 00:08:29 +0700

    progress

--- test/test-travels.js
+++ test/test-travels.js
@@ -4,8 +4,8 @@ describe('When parsing the Travels spreadsheet', function() {
   var travels;
   before(function(done) {
     var filePath = 'test/assets/Travels.xlsx';
-    stoicXlsx.parseXlsx(filePath, function() {
-      console.log(arguments);
+    stoicXlsx.parseXlsx(filePath, {evaluateFmt: false, skipEmptyCells: true}, function(e, sheets) {
+      travels = sheets;
       done();
     });
   });
