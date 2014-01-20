a25e838ce9acf518fb2ab62649d8fb29c765894a
Hugues Malphettes <hmalphettes@gmail.com>
2014-01-20 07:53:29 +0700

    First support for parsing dates.

--- test/test-travels.js
+++ test/test-travels.js
@@ -4,12 +4,17 @@ describe('When parsing the Travels spreadsheet', function() {
   var travels;
   before(function(done) {
     var filePath = 'test/assets/Travels.xlsx';
-    stoicXlsx.parseXlsx(filePath, {evaluateFmt: false, skipEmptyCells: true}, function(e, sheets) {
+    stoicXlsx.parseXlsx(filePath, {evaluateFmt: true, skipEmptyCells: true, skipRawnf: false}, function(e, sheets) {
       travels = sheets;
       done();
     });
   });
-  it('does it', function() {
-
+  it('Must have parsed 6 sheets', function() {
+    expect(Object.keys(travels)).to.deep.equal(['Applications', 'Companies', 'Fields', 'Hotels', 'Objects','Restaurants']);    
+  });
+  it('Must find a javascript date where expected', function() {
+    var l = travels.Restaurants.values[0].length;
+    var aDate = travels.Restaurants.values[1][l-5];
+    expect(aDate).to.be.instanceOf(Date);
   });
 });
