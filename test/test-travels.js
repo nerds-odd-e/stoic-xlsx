873e37fe0689521a8f7e73364f906c28bda40433
Hugues Malphettes <hmalphettes@gmail.com>
2014-02-15 19:25:58 +0800

    Parse the custom properties

--- test/test-travels.js
+++ test/test-travels.js
@@ -1,11 +1,15 @@
 var expect = require('chai').expect;
 var stoicXlsx = require('../lib/stoic-xlsx');
 describe('When parsing the Travels spreadsheet', function() {
-  var travels;
+  var travels, name, spid, url, folder;
   before(function(done) {
     var filePath = 'test/assets/Travels.xlsx';
     stoicXlsx.parseXlsx(filePath, function(e, spreadsheet) {
       travels = spreadsheet.sheets;
+      name = spreadsheet.name;
+      spid = spreadsheet.spid;
+      url = spreadsheet.url;
+      folder = spreadsheet.folder;
       done();
     });
   });
@@ -14,14 +18,22 @@ describe('When parsing the Travels spreadsheet', function() {
   });
   it('Must find a javascript date where expected', function() {
     var l = travels.Restaurants.values[0].length;
-    var aDate = travels.Restaurants.values[1][l-5];
+    var aDate = travels.Restaurants.values[1][l-6];
     expect(aDate).to.be.instanceOf(Date);
     expect(isNaN(aDate.valueOf())).to.equal(false);
   });
   it('Must find a raw number format where expected', function() {
     var l = travels.Restaurants.values[0].length;
-    var aDateNF = travels.Restaurants.numberFormats[1][l-5];
-    expect(aDateNF).to.equal('m/d/yyyy h:mm:ss');
+    var aDateNF = travels.Restaurants.numberFormats[1][l-6];
+    expect(aDateNF).to.equal('M/d/yyyy H:mm:ss');
+  });
+  it('Must have read the custom properties', function() {
+    expect(name).to.equal('Travels');
+    expect(spid).to.equal('0Ah3dPnCQn7k4dHNzY3JidkdnV1liQzdrVnB4WjZJdUE');
+    expect(url).to.equal('https://docs.google.com/a/sutoiku.com/spreadsheet/ccc?key=tsscrbvGgWYbC7kVpxZ6IuA');
+    expect(folder).to.deep.equal({
+      id: '0Bx3dPnCQn7k4N2tTNUk5bVVyVFk',
+      url: 'https://docs.google.com/a/sutoiku.com/open?id=0Bx3dPnCQn7k4N2tTNUk5bVVyVFk' });
   });
 });
 describe('When parsing the SmallDateFormats spreadsheet', function() {
